435468cb22a993a4259a44cb7c7551c89266f0b1
aishwarya <aishwaryashelke88@gmail.com>
2021-04-08 09:53:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ import React from 'react';
+ import Products from './components/Products/Products';
+ import Navbar from './components/navbar/Navbar';
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            {/* <Products/> */}
+        </div>
+    )
+}
+
+export default App;
